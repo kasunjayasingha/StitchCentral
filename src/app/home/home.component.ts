3acee060c8da9f0cd6a2927a719ba5ea48173b5a
Kasun Jayasinghe <74439310+kasunjayasingha@users.noreply.github.com>
2023-07-09 02:42:02 +0530

    Add about section to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faHouseCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { faShirt } from '@fortawesome/free-solid-svg-icons';
 import { faRug } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'home',
@@ -12,4 +13,5 @@ export class HomeComponent {
   faHouseCircleCheck = faHouseCircleCheck;
   faShirt = faShirt;
   faRug = faRug;
+  faCircleCheck = faCircleCheck;
 }
