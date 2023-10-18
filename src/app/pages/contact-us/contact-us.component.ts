f3e2684eafce3498eb63b9452ed535fae77a37c6
Sewmini Wijesingha <sew.wijesingha75@gmail.com>
2023-10-18 23:18:21 +0530

    contac-us

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,16 +1,15 @@
-import {Component} from '@angular/core';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
-import {faPhone} from '@fortawesome/free-solid-svg-icons';
-import {faLocationDot} from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent {
   faEnvelope = faEnvelope;
   faPhone = faPhone;
   faLocationDot = faLocationDot;
-
 }
