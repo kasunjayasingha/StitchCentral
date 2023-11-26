04957c474a01b5f34d5882e01521ce426f1a6533
Kasun Jayasinghe <kasun_t@epiclanka.net>
2023-11-26 18:55:45 +0530

    Appointment file upload process ongoing

--- src/app/DTO/AppointmentsDTO.ts
+++ src/app/DTO/AppointmentsDTO.ts
@@ -1,5 +1,6 @@
 import {ClientSampleDTO} from "./clientSampleDTO";
 import {CustomerDTO} from "./customerDTO";
+import {FileHandleModel} from "./file.handle.model";
 
 export class AppointmentsDTO {
   id: number;
@@ -10,10 +11,11 @@ export class AppointmentsDTO {
   description: string;
   client_sample: ClientSampleDTO;
   customer: CustomerDTO;
+  file: FileHandleModel;
 
 
   constructor(id: number, customer_Id: number, appointment_date: Date, type: string,
-              status: string, description: string, client_sample: ClientSampleDTO, customer: CustomerDTO) {
+              status: string, description: string, client_sample: ClientSampleDTO, customer: CustomerDTO, file: FileHandleModel) {
     this.id = id;
     this.customer_Id = customer_Id;
     this.appointment_date = appointment_date;
@@ -22,6 +24,7 @@ export class AppointmentsDTO {
     this.description = description;
     this.client_sample = client_sample;
     this.customer = customer;
+    this.file = file;
   }
 
 }
